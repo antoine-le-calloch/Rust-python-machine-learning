cc57343b5a594f5f7b0d93dbbdf3e7d2153ab0c3
antoine-le-calloch <antoine.le-calloch@etu.univ-cotedazur.fr>
2024-07-28 22:09:08 -0700

    Use rust matrix_multiply.

--- rust-machine-learning/src/main.rs
+++ rust-machine-learning/src/main.rs
@@ -1,3 +1,12 @@
+use rust_machine_learning::matrix_multiply;
+
 fn main() {
-    println!("Hello, world!");
+    let n = 2;
+    let a = vec![1.0, 2.0, 3.0, 4.0];
+    let b = vec![5.0, 6.0, 7.0, 8.0];
+    let mut c = vec![0.0; n * n];
+    
+    matrix_multiply(a.as_ptr(), b.as_ptr(), c.as_mut_ptr(), n);
+
+    println!("{:?}", c);
 }
